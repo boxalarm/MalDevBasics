39f1ae3222dadd628cc35fb46434ac78f4b8270d
boxalarm <16870969+boxalarm@users.noreply.github.com>
2025-12-12 20:14:23 -0500

    Create testdll.c

---
+++ testdll.c
@@ -0,0 +1,37 @@
+// DLL that can be used for testing - it'll pop a message box when successfully
+// injected into a process
+
+#include "framework.h"
+#include <Windows.h>
+#include <stdio.h>
+#include <stdlib.h> // required for _countof
+
+void message(void) {
+    // First get the current PID
+    DWORD pid = GetCurrentProcessId();
+    WCHAR buffer[64] = { 0 };
+    // Convert DWORD to wide string which MessageBoxW requires
+    swprintf(buffer, _countof(buffer), L"%lu", pid);
+
+    // Call MessageBox to display the current process ID
+    MessageBox(NULL, buffer, L"Procces ID", MB_OK | MB_ICONWARNING);
+    return;
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        message();
+        break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
